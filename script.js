ba244a587b63e6f40dea54bee325ca9b2fbdebd5
Jarrod Finn <jarrod.p.finn@gmail.com>
2019-12-16 22:39:19 -0500

    Connected .js file to pull information

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ $(document).ready(function () {
     $(".saveBtn").on("click", function () {
         var value = $(this).siblings(".description").val();
         var time = $(this).siblings(".hour").text();
-        console.log(this);
         localStorage.setItem(time, value);
     });
-    $('#currentDay').text(moment());
+    $('#currentDay').text(moment().format('LLLL'));
 });
